4fc61e10f631bcb820bbf00aa6b3b917f26c06a1
divaamahajan <dm.divya.mahajan@gmail.com>
2024-02-20 18:02:51 -0800

    shuffle

--- deck.go
+++ deck.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
 //  Create a new type of "deck"
 //	which is an slice of strings
@@ -26,4 +33,57 @@ func newDeck() deck {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	    // Open a file for writing. If the file doesn't exist, it will be created.
+	f, err := os.Create(filename)
+    if err!= nil {
+        return err
+    }
+    defer f.Close()// Ensure the file is closed when the function returns
+
+    // Write data to the file
+    _, err = f.WriteString(d.toString())
+    if err!= nil {
+        return err
+    }
+    return nil
+}
+
+func newDeckFromFile(filename string) (deck, error) {
+	var cards deck
+    f, err := os.Open(filename)
+    if err!= nil {
+		fmt.Println(err)
+		os.Exit(1)
+        // return newDeck(), err
+    }
+    defer f.Close()
+    scanner := bufio.NewScanner(f)
+    for scanner.Scan() {
+        line := scanner.Text()
+        // Split the line by commas
+        s := strings.Split(line, ",")
+		
+        // Append each term to the slice
+        for _, term := range s {
+            cards = append(cards, term)
+        }
+    }
+    return cards, nil
+}
+
+func (d deck) shuffleCards() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+	for i := range d{
+		j := r.Intn(len(d)-1)
+        d[i], d[j] = d[j], d[i]
+	}
+
 }
--- main.go
+++ main.go
@@ -3,8 +3,13 @@ package main
 import "fmt"
 func main() {
 	cards:= newDeck()
-	hand, remainingCards := deal(cards, 5)
-	hand.print()
-	fmt.Println("--------------------------------")
-	remainingCards.print()
+	// cards,error :=newDeckFromFile("cards.txt")
+	cards.shuffleCards()
+	// cards.print()
+	fmt.Println(cards.saveToFile("cards.txt"))
+	// fmt.Println(cards.toString(), error)
+	// hand, remainingCards := deal(cards, 5)
+	// hand.print()
+	// fmt.Println("--------------------------------")
+	// remainingCards.print()
 }
